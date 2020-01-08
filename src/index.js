c351823e5e5e68f097ee02fff34912357760ce47
unknown <reihanagam>
2020-01-08 11:28:04 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const auth = require('./routes/auth')
 const product = require('./routes/product')
 const Route = express.Router()
 
 Route
+    .use('/api/v1/auth', auth)
     .use('/api/v1/products', product)
 
 module.exports = Route
