1fb86fbca8d40267fc5658c82448e0e6f3767a7d
bayuyuhartono <bayuyuhartono@gmail.com>
2020-01-09 22:42:24 +0700

    jwt-helper

---
+++ src/helper/jwt.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken')
+const misc = require('../helper/misc')
+const User = require('../models/User')
+
+module.exports = {
+    CheckToken: async (req, response, next) => {
+        try {
+            const email = req.headers.email
+            const checkUser = await User.checkUser(email)
+            if(checkUser.length === 0) {
+                return misc.response(response, 500, true, 'Oops!, email not exists')
+            }
+
+            const token = req.headers.authorization.split(' ')[1]
+            const decodeToken = jwt.verify(token, process.env.JWT_KEY)
+            if (decodeToken && decodeToken.email === email) {
+                next()
+            } else {
+                return misc.response(response, 500, true, 'Invalid "Email" Token')
+            }
+        } catch (err) {
+            return misc.response(response, 500, true, 'Invalid Token')
+        }
+    },
+}
--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ const auth = require('./routes/auth')
 const product = require('./routes/product')
 const profile = require('./routes/profile')
 const shipment = require('./routes/shipment')
+const jwtCheck = require('./helper/jwt')
 const Route = express.Router()
 
 Route
     .use('/api/v1/auth', auth)
     .use('/api/v1/products', product)
     .use('/api/v1/profile', profile)
-    .use('/api/v1/shipment', shipment)
+    .use('/api/v1/shipment', jwtCheck.CheckToken, shipment)
 
 module.exports = Route
