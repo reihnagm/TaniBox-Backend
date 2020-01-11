57b1657067be314bbef6810e0e9e9f7f7b62451b
bayuyuhartono <bayuyuhartono@gmail.com>
2020-01-11 22:01:46 +0700

    payment-1

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -2,6 +2,6 @@ const express = require('express')
 const payment = require('../controllers/payment')
 const Route = express.Router()
 
-Route.get('/notification', payment.receiveNotif)
+Route.post('/notification', payment.receiveNotif)
 
 module.exports = Route
