ccb542fee0f36cbc37cfbf924f86234330a3696c
bayuyuhartono <bayuyuhartono@gmail.com>
2020-01-11 22:39:27 +0700

    fix-jwt-1

--- src/helper/jwt.js
+++ src/helper/jwt.js
@@ -1,10 +1,12 @@
+require('dotenv').config()
 const jwt = require('jsonwebtoken')
 const misc = require('../helper/misc')
-const User = require('../models/User')
 
 module.exports = {
     CheckToken: async (req, response, next) => {
         try {
+            const token = req.headers.authorization.split(' ')[1]
+            const decodeToken = jwt.verify(token, process.env.JWT_KEY)
             if (decodeToken) {
                 next()
             }
