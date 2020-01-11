e8ba120216741db9905065ee0a8fa565a1786842
bayuyuhartono <bayuyuhartono@gmail.com>
2020-01-11 22:16:41 +0700

    payment1.1

--- src/controllers/payment.js
+++ src/controllers/payment.js
@@ -2,7 +2,7 @@ const misc = require('../helper/misc')
 
 module.exports = {
     receiveNotif: async (request, response) => {
-        console.log(request);
+        console.log(request.body);
         return misc.response(response, 200, false, 'Successfull')
     }
 }
