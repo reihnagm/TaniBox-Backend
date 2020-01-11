2a10fb82130db5836d9e6c3b0c0a9953aa5baa0c
bayuyuhartono <bayuyuhartono@gmail.com>
2020-01-11 20:39:42 +0700

    test-paymet-1.0

--- src/controllers/payment.js
+++ src/controllers/payment.js
@@ -2,7 +2,7 @@ const misc = require('../helper/misc')
 
 module.exports = {
     receiveNotif: async (request, response) => {
-        console.log(request.body);
+        console.log(request);
         return misc.response(response, 200, false, 'Successfull')
     }
 }
