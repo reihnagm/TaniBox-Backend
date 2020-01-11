5776ac7b2025b586d0038a7df6332ce326951cb4
bayuyuhartono <bayuyuhartono@gmail.com>
2020-01-11 22:33:43 +0700

    fix-jwt

--- src/helper/jwt.js
+++ src/helper/jwt.js
@@ -5,7 +5,9 @@ const User = require('../models/User')
 module.exports = {
     CheckToken: async (req, response, next) => {
         try {
-            next()
+            if (decodeToken) {
+                next()
+            }
         } catch (err) {
             return misc.response(response, 500, true, 'Invalid Token')
         }
