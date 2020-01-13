7499b0ecf16c08bf4654b727bc72d03f99e825e3
unknown <reihanagam>
2020-01-13 22:33:18 +0700

    update

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -225,6 +225,8 @@ module.exports = {
             const salt = await bcrypt.genSalt(10);
             const passwordHash = await bcrypt.hash(old_password, salt)
 
+            console.log(typeof passwordHash, typeof db_password)
+
             if(passwordHash === db_password[0].password) {
                 error = true
                 throw new Error('Oops, Password cannot same with old password')
